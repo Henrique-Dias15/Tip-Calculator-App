17338fa24aa7d172febfce866a9f10fa20053402
Henrique Dias <henriquejnd@proton.me>
2023-10-16 21:34:04 -0300

    Select Tip Buttons

--- tipCalculatorApp/src/App.jsx
+++ tipCalculatorApp/src/App.jsx
@@ -16,6 +16,17 @@ function App() {
           <input type="number" id='bill' placeholder='$'/>
           </div>
 
+          <div className='inputTip'>
+            <p>Select Tip %</p>
+            <div className='buttons'>
+            <input type="button" id='tip5' value={5+'%'}/>
+            <input type="button" id='tip10' value={10+'%'}/>
+            <input type="button" id='tip15' value={15+'%'}/>
+            <input type="button" id='tip25' value={25+'%'}/>
+            <input type="button" id='tip50' value={50+'%'}/>
+            <input type="number" id='tipCustom' placeholder={'Custom'}/>
+            </div>
+          </div>
         </div>
 
       </div>
