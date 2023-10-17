1013cece9972bddb54531cebd169735bec3d578e
Henrique Dias <henriquejnd@proton.me>
2023-10-16 22:00:46 -0300

    input Numero de Pessoas

--- tipCalculatorApp/src/App.jsx
+++ tipCalculatorApp/src/App.jsx
@@ -1,37 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
+      <h1>
+        SPLI <br /> TTER
+      </h1>
 
-      <h1>SPLI <br/> TTER</h1>
-
-      <div className='Main'>
-
-        <div className='Inputs'>
-
-          <div className='inputBill'>
-          <label htmlFor='bill'>Bill</label>
-          <input type="number" id='bill' placeholder='$'/>
+      <div className="Main">
+        <div className="Inputs">
+          <div className="inputBill">
+            <label htmlFor="bill">Bill</label>
+            <input type="number" id="bill" />
           </div>
 
-          <div className='inputTip'>
+          <div className="inputTip">
             <p>Select Tip %</p>
-            <div className='buttons'>
-            <input type="button" id='tip5' value={5+'%'}/>
-            <input type="button" id='tip10' value={10+'%'}/>
-            <input type="button" id='tip15' value={15+'%'}/>
-            <input type="button" id='tip25' value={25+'%'}/>
-            <input type="button" id='tip50' value={50+'%'}/>
-            <input type="number" id='tipCustom' placeholder={'Custom'}/>
+            <div className="buttons">
+              <input type="button" id="tip5" value={5 + "%"} />
+              <input type="button" id="tip10" value={10 + "%"} />
+              <input type="button" id="tip15" value={15 + "%"} />
+              <input type="button" id="tip25" value={25 + "%"} />
+              <input type="button" id="tip50" value={50 + "%"} />
+              <input type="number" id="tipCustom" placeholder={"Custom"} />
             </div>
           </div>
-        </div>
 
+          <div className="inputPeople">
+            <label htmlFor="people">Number of People</label>
+            <input type="number" id="people" />
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
